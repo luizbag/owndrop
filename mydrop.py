f6f4ba9f50625404d9118d6f7dd26293d4cb6b03
Luiz Gavioli <luizgavioli@cofcointernational.com>
2021-09-07 09:31:40 +0100

    Starting

---
+++ mydrop.py
@@ -0,0 +1,27 @@
+import os
+
+DIR = ".\\drop"
+
+file_info = {}
+
+def main():
+    refresh = True
+    while(refresh):
+        files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(DIR)) for f in fn]
+
+        for f in files:
+            new_time = os.path.getmtime(f)
+            if f in file_info:
+                if file_info[f] != new_time:
+                    print("File " + f + " modified")
+            else:
+                print(f + " is a new file")
+            file_info[f] = os.path.getmtime(f)
+
+        print(file_info)
+        i = input("refresh (Y/n): ")
+        refresh = i != 'n'
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
